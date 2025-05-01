9a3611c80bc363ef87810d1493653da1aeac62bd
Arafat Hossain Ankon <159070473+ankon35@users.noreply.github.com>
2025-05-01 21:47:20 +0600

    Add files via upload

---
+++ cv_tutor/backend/env.py
@@ -0,0 +1,8 @@
+GOOGLE_API_KEY = "AIzaSyDNMyGAUfhGqmxGflbbQeKHzSlHsPMyYHs"
+MODEL = "gemini-1.5-flash"
+
+CV_FILE_DIR = "./files/resume/"
+JD_FILE_DIR = "./files/jd/"
+
+CV_FILE = "./files/resume/resume.pdf"
+JD_FILE = "./files/jd/jd.txt"
